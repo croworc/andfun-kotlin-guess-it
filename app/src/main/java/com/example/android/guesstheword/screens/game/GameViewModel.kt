b1c08bd1ee2c9466a7c74935a47a51a9d1924129
ikrotzky <ikr@croworc.com>
2019-03-26 10:46:51 +0100

    [Step03-Ex] feat: Use LiveData for the word & the score
    GameViewModel wraps these two data items into MutableLifeData, the
    GameFragment has the UI views that will display these items observe
    the ViewModel LifeData. As the views will be updated automatically,
    there's no more need to manually update the views. All we have to do is
    to change the data in the buttons' OnClickHandler_s.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
@@ -24,23 +25,25 @@ import androidx.lifecycle.ViewModel
  */
 class GameViewModel : ViewModel() {
 
-    // TODO (01) Wrap word and score in MutableLiveData
+    // COMPLETED (01) Wrap word and score in MutableLiveData
     // The current word
-    var word = ""
+    val word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
-    // TODO (02) Change references to score and word to score.value and word.value and add the
-    // require null safety checks
+    val score = MutableLiveData<Int>()
+
+    // COMPLETED (02) Change references to score and word to score.value and word.value and add the
+    // required null safety checks
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
 
     init {
-        resetList()
-        nextWord()
-        // TODO (03) Initialize score.value to 0
+        resetList() // creates a new mutable list and shuffles it
+        nextWord()  // retrieves the first word from that list
+        // COMPLETED (03) Initialize score.value to 0
+        score.value = 0
     }
 
     /**
@@ -79,21 +82,21 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            // gameFinished() should happen here
+             // gameFinished() // should happen here
         } else {
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        score.value = score.value?.dec() // decrement by 1
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = score.value?.inc() // increment by 1
         nextWord()
     }
 }
