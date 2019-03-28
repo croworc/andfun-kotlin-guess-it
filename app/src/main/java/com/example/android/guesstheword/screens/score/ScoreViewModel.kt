a5a7fdda03d5bb0f62a4c73b632b67e600ce8e2b
ikrotzky <ikr@croworc.com>
2019-03-28 07:57:23 +0100

    [Step07-Ex] feat: Add ScoreViewModel class stub w/ constructor param
    Takes in the final score as an Int

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -16,7 +16,19 @@
 
 package com.example.android.guesstheword.screens.score
 
-// TODO (01) Create the ScoreViewModel class and have it take in an integer constructor parameter
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val LOG_TAG = "ScoreViewModel"
+
+// COMPLETED (01) Create the ScoreViewModel class and have it take in an integer constructor parameter
 // called finalScore
-// TODO (06) Add a LiveData for the score and the play again event, using the best practices for
-// encapsulation and event handling that you've learned
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+    init {
+        Log.d(LOG_TAG, "Final score is: $finalScore")
+    }
+
+    // TODO (06) Add a LiveData for the score and the play again event, using the best practices for
+    // encapsulation and event handling that you've learned
+
+}
