395aa03c4751ca2c1c4e5fedc6c5c2d2f759cb9f
ikrotzky <ikr@croworc.com>
2019-04-03 20:43:57 +0200

    [Step10-Ex] docs: Mark TODO (2) as completed in GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -50,11 +50,11 @@ class GameViewModel : ViewModel() {
         get() = _currentTime
 
     // COMPLETED (01) Create a new LiveData called currentTimeString.
+    // COMPLETED (02) Use Transformation.map to take the number output from currentTime, and transform
+    // it into a String using DateUtils.
     val currentTimeString = Transformations.map(currentTime) { time ->
         DateUtils.formatElapsedTime(time)
     }
-    // TODO (02) Use Transformation.map to take the number output from currentTime, and transform
-    // it into a String using DateUtils.
 
     // The current word
     private val _word = MutableLiveData<String>()
